2788900c7ae3a7babc26688d21caf9a3a1d051e8
Aleksei Cherepanov <mrneumann888@gmail.com>
2018-03-16 19:33:04 +0300

    Check git push from studio

--- app/src/main/java/com/example/mrneumann/vibcam/MainActivity.kt
+++ app/src/main/java/com/example/mrneumann/vibcam/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.mrneumann.vibcam
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main)
     }
 }
