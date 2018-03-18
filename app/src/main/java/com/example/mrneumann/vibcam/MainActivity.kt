b12e023b68ed8bff6022ac7e764bd069d2abad65
Aleksei Cherepanov <mrneumann888@gmail.com>
2018-03-18 13:16:57 +0300

    Camera Permition

--- app/src/main/java/com/example/mrneumann/vibcam/MainActivity.kt
+++ app/src/main/java/com/example/mrneumann/vibcam/MainActivity.kt
@@ -1,15 +1,56 @@
 package com.example.mrneumann.vibcam
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.view.WindowManager
+import android.widget.Toast
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
-
+    private val MY_PERMISSIONS_REQUEST_CAMERA = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //fullscreen
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main)
+
+        //CAMERA Permition
+        if (ContextCompat.checkSelfPermission(this@MainActivity, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this@MainActivity,
+                    arrayOf(Manifest.permission.CAMERA),
+                    MY_PERMISSIONS_REQUEST_CAMERA)
+        }
     }
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<String>,
+            grantResults: IntArray
+    ) {
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_CAMERA -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    //its OK
+                } else {
+                    //TODO отключить функциональность
+                }
+                return
+            }
+
+            else -> {
+                // Ignore all other requests.
+            }
+        }
+    }
+
 }
